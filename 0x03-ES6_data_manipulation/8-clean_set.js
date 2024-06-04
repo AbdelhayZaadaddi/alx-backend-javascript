54bb3b66c507739309e7e7bba8bf55536dd33eae
abdelhay zaadaddi <abdezaadaddi06@gmail.com>
2024-06-04 14:56:46 -0700

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,18 @@
-export default function cleanSet(set, startString) {
-  const result = [];
-  if (!startString || typeof startString !== "string") {
-    return "";
+function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  for (const value of set) {
-    if (typeof value === "string" && value.startsWith(startString)) {
-      if (value.length > startString.length) {
-        result.push(value.slice(startString.length));
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
       }
     }
   }
-
-  return result.join("-");
+  return parts.join('-');
 }
+
+export default cleanSet;
